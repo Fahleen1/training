a54a1cb6aaaf4246e8b29f80f5a0161b899972dd
Fahleen1 <mishaarif14@gmail.com>
2024-11-29 12:52:01 +0500

    RESOLVED: Added prettier and removed comments

--- currency-converter/src/App.tsx
+++ currency-converter/src/App.tsx
@@ -1,15 +1,13 @@
-import './App.css'
-import Form from './components/Form'
-import MainHeading from './components/MainHeading'
+import Form from './components/Form';
+import MainHeading from './components/MainHeading';
 
 function App() {
- 
   return (
     <div>
-      <MainHeading name="Currency Converter"/>
-      <Form/>
+      <MainHeading name="Currency Converter" />
+      <Form />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
