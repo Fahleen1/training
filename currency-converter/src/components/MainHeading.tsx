d327f9d43fb21672234237cddd256474a9a43952
Fahleen Arif <106329502+Fahleen1@users.noreply.github.com>
2024-12-12 14:36:22 +0500

    Delete currency-converter directory

--- currency-converter/src/components/MainHeading.tsx
+++
@@ -1,6 +0,0 @@
-interface IMainHeading {
-  name: string;
-}
-export default function MainHeading({ name }: IMainHeading) {
-  return <h1 className="text-4xl px-2 text-center font-bold">{name}</h1>;
-}
